fc003c26bd6a93293a02c37298b857bf18c3072f
Sean <simpson.sean@outlook.com>
2021-07-28 17:31:15 -0700

    WORKDONE: second passing CRUD test: GET

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import TREK from '../lib/models/stapi-model.js';
 
 
 // CRUD
@@ -18,7 +19,7 @@ describe('stapi routes', () => {
     return setup(pool);
   });
 
-  it('creates stapi object', async () => {
+  it('creates a character', async () => {
       const character = { name: 'Captian Kirk', species: 'Human', faction: 'Starfleet'};
       const res = await request(app).post('/api/v1/trek_characters').send(character);
 
@@ -28,6 +29,17 @@ describe('stapi routes', () => {
       })
   });
 
+  it('gets character by id', async () => {
+    const character = await TREK.insert({
+      name: 'Captain Kirk',
+      species: 'Human',
+      faction: 'Starfleet',
+    });
+
+    const res = await request(app).get(`/api/v1/trek_characters/${character.id}`);
+
+    expect(res.body).toEqual(character);
+  });
 
 }); // <--- END PARENT CODE BLOCK
 
